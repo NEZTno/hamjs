0411ed4bb9448a4b0fad493fd243572e841aaab8
Son Thanh Vo <thanhsonvo95@gmail.com>
2017-03-25 15:47:08 +0100

    it can manipulate some html now, oh shit

--- ham.js
+++ ham.js
@@ -1,18 +1,17 @@
 (function(window){
     "use strict";
 
-    var ham = function ham(parameter){
+    var ham = function(parameter){
         return new Library(parameter);
     }
 
     var Library = function(parameter){
-        var elements = document.querySelectorAll(parameter);
-        for(var i=0;i<elements.length;i++){
+        const elements = document.querySelectorAll(parameter);
+        console.log("Garry found " + elements.length + " pig(s)");
+        for(var i=0;i<elements.length;i++)
             console.log(elements[i]);
-        }
         this.pigs = elements.length;
         this.cookedHam = 0;
-        console.log("Garry found " + elements.length + " pig(s)");
         return this;
     }
 
@@ -37,12 +36,45 @@
             return this;
         },
         hunt: function(parameter){
-            var hunting = document.querySelectorAll(parameter);
-            for(var i=0;i<hunting.length;i++){
-                console.log(hunting[i]);
-            }
-            this.pigs += hunting.length;
+            const hunting = document.querySelectorAll(parameter);
             console.log("Garry slaugthered " + hunting.length + " pig(s)");
+            for(var i=0;i<hunting.length;i++)
+                console.log(hunting[i]);
+            this.pigs += hunting.length;
+            return this;
+        },
+        hidePigs: function(parameter){
+            const pigs = document.querySelectorAll(parameter);
+            console.log("Garry hides his pigs");
+            for (var i = 0; i < pigs.length; i++) {
+                pigs[i].style.display = 'none';
+            }
+            return this;
+        },
+        showPigs: function(parameter){
+            const pigs = document.querySelectorAll(parameter);
+            console.log("Garry shows his pigs");
+            for (var i = 0; i < pigs.length; i++) {
+                pigs[i].style.display = 'block';
+            }
+        },
+        piggyBath: function(parameter){
+            const pigs = document.querySelectorAll(parameter);
+            console.log("Garry gives his pigs a nice bath");
+            for (var i = 0; i < pigs.length; i++) {
+                pigs[i].innerHTML = "";
+            }
+        },
+        throwDirt: function(parameter, text){
+            if(text){
+                const pigs = document.querySelectorAll(parameter);
+                console.log("Garry plays around with his pigs in the dirt");
+                for (var i = 0; i < pigs.length; i++) {
+                    pigs[i].innerHTML = text;
+                }
+            }else{
+                console.log("It is a shiny day, Garry cant seem to find any dirt to throw at his pigs");
+            }
             return this;
         }
     }
