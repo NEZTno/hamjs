5887b67a764b05915d8c3d1c0b1a646d72678665
Son Thanh Vo <thanhsonvo95@gmail.com>
2017-03-25 10:16:03 +0100

    cleaned it up for readability

--- ham.js
+++ ham.js
@@ -2,37 +2,43 @@
     "use strict";
 
     var ham = function ham(parameter){
+        return new Library(parameter);
+    }
+
+    var Library = function(parameter){
         var elements = document.querySelectorAll(parameter);
+        this.pigs = elements.length;
+        this.cookedHam = 0;
         console.log("Garry found " + elements.length + " pig(s)");
-        return {
-            pigs: elements.length,
-            cookedHam: 0,
-            cook: function(){
-                if(this.pigs > 0){
-                    this.pigs = this.pigs - 1;
-                    this.cookedHam = this.cookedHam + 1;
-                    console.log("Garry makes cooked ham");
-                }else{
-                    console.log("Garry dont have any pigs");
-                }
-                return this;
-            },
-            eat: function(){
-                if(this.cookedHam > 0){
-                    this.cookedHam = this.cookedHam - 1;
-                    console.log("Garry eats the cooked ham");
-                }else{
-                    console.log("Garry can't seem to find his cooked ham");
-                }
-                return this;
-            },
-            hunt: function(parameter){
-                var hunting = document.querySelectorAll(parameter);
-                this.pigs = this.pigs + hunting.length;
-                console.log("Garry slaugthered " + hunting.length + " pig(s)");
-                return this;
+        return this;
+    }
+
+    Library.prototype = {
+        cook: function(){
+            if(this.pigs > 0){
+                this.cookedHam += 1;
+                this.pigs -= 1;
+                console.log("Garry makes cooked ham");
+            }else{
+                console.log("Garry dont have any pigs");
             }
-        };
+            return this;
+        },
+        eat: function(){
+            if(this.cookedHam > 0){
+                this.cookedHam = this.cookedHam - 1;
+                console.log("Garry eats the cooked ham");
+            }else{
+                console.log("Garry can't seem to find his cooked ham");
+            }
+            return this;
+        },
+        hunt: function(parameter){
+            var hunting = document.querySelectorAll(parameter);
+            this.pigs += hunting.length;
+            console.log("Garry slaugthered " + hunting.length + " pig(s)");
+            return this;
+        }
     }
 
     if(!window.ham){
